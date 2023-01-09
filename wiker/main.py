ca6e92983036efe186a5e0de0430867e3fb0a01d
anorprogrammer <anorprogrammer1127@gmail.com>
2023-01-09 13:16:18 +0500

    Update main.py

--- wiker/main.py
+++ wiker/main.py
@@ -1,2 +1,18 @@
-def main():
-    print("Hello, World!")
+import re
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+class Wiker:
+    """
+    some class docs
+    """
+    def __init__(self):
+        """
+        some constructor docs
+        """
+        pass
